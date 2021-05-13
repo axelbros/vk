4fb7a75cd98ea5d87fb08db6ef7fa91845920ec7
Alexander Ivankov <ffmoneyff@gmail.com>
2021-05-13 20:50:13 +0300

    v1'

---
+++ src/main/java/ru/netology/domain/Content.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class Content {
+    private String text;
+    private String imageUrl;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+public class Post {
+
+    private int idOwner;
+    private int idSender;
+    private int idPost;
+    private String nameSender;
+    private String date;
+    private Content content;
+    private String imageUrl;
+    private LikesInfo likesInfo;
+    private CommentsInfo commentsInfo;
+private int views;
+private int reposts;
+
+}
