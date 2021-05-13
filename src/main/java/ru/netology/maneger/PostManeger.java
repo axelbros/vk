254e278ba3a82fcfdc1c28adf9b9146af66017f2
Alexander Ivankov <ffmoneyff@gmail.com>
2021-05-13 21:19:52 +0300

    addedManager

---
+++ src/main/java/ru/netology/maneger/PostManeger.java
@@ -0,0 +1,14 @@
+package ru.netology.maneger;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, int ownerOnly, int count,int offset) {
+        return posts;
+    }
+
+    public void delete(int postId, int ownerId) {
+    }
+}
